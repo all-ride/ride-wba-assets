1ae783b30e4d59ab8a3352246005090165795b77
Kaya <kayalion@kayalion.net>
2016-02-21 01:18:32 +0100

    use variable instead of getting again

--- src/ride/web/base/controller/AssetController.php
+++ src/ride/web/base/controller/AssetController.php
@@ -589,10 +589,11 @@ class AssetController extends AbstractController {
         ));
         foreach ($styles as $style) {
             $imageStyleImage = $asset->getStyle($style->getSlug());
+
             $form->addRow('style-' . $style->getSlug(), 'image', array(
                 'path' => $assetComponent->getDirectory(),
                 'attributes' => array(
-                    'data-id' => ($imageStyleImage) ? $asset->getStyle($style->getSlug())->getId() : null,
+                    'data-id' => $imageStyleImage ? $imageStyleImage->getId() : null,
                 ),
             ));
         }
